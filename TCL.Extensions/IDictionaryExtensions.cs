8c4fb72829d3b4885d06aba8ae8f1683931c91e5
gunr2171 <kevin@kevin-hunter.net>
2015-01-18 00:26:08 -0500

    adding the extension methods

---
+++ TCL.Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCL.Extensions
+{
+    public static class IDictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            TValue outValue;
+            if (dictionary.TryGetValue(key, out outValue))
+            {
+                return outValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        public static TValue GetValueOrNull<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : class
+        {
+            return dictionary.GetValueOrDefault(key, null);
+        }
+    }
+}
