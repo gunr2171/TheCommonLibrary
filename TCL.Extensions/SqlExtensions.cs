c85a18a64d41426017bececb7688c1b3783c728f
gunr2171 <kevin@kevin-hunter.net>
2015-02-07 19:04:27 -0500

    more files

---
+++ TCL.Extensions/SqlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCL.Extensions
+{
+    public static class SqlExtensions
+    {
+        public static SqlParameterCollection AddParam(this SqlParameterCollection collection, string parameterName, object value)
+        {
+            if(valu)
+        }
+    }
+}
