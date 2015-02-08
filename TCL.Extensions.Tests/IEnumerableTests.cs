1c6b91b99dd37ccbdd552ebfe333708410bd54b1
gunr2171 <kevin@kevin-hunter.net>
2015-02-07 19:23:33 -0500

    adding in the extensions unit tests

---
+++ TCL.Extensions.Tests/IEnumerableTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TCL.Extensions;
+
+namespace TCL.Tests.Extensions
+{
+    [TestFixture]
+    public class IEnumerableTests
+    {
+        [Test]
+        public void PickRandom_Valid()
+        {
+            List<int> intList = Enumerable.Range(1, 10).ToList();
+
+            var randomElement = intList.PickRandom();
+
+            bool isInside = intList.Contains(randomElement);
+
+            Assert.IsTrue(isInside);
+        }
+    }
+}
