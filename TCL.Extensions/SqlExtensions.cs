0920e53f5b05287a5c8a0e479451ca2e45abb32e
gunr2171 <kevin@kevin-hunter.net>
2015-04-19 11:34:17 -0400

    adding a MySql version of AddParam

--- TCL.Extensions/SqlExtensions.cs
+++ TCL.Extensions/SqlExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -20,6 +21,25 @@ namespace TCL.Extensions
         /// <param name="parameterName">The name of the parameter. Like "@ParamName".</param>
         /// <param name="value">The value of the parameter.</param>
         public static void AddParam(this SqlParameterCollection collection, string parameterName, object value)
+        {
+            if (value == null)
+            {
+                collection.AddWithValue(parameterName, DBNull.Value);
+            }
+            else
+            {
+                collection.AddWithValue(parameterName, value);
+            }
+        }
+
+        /// <summary>
+        /// Adds a parameter to the parameter collection with it's accompanying value.
+        /// If the value passed in is null it will be converted to DBNull.Value.
+        /// </summary>
+        /// <param name="collection">The collection to add the parameter to.</param>
+        /// <param name="parameterName">The name of the parameter. Like "@ParamName".</param>
+        /// <param name="value">The value of the parameter.</param>
+        public static void AddParam(this MySqlParameterCollection collection, string parameterName, object value)
         {
             if (value == null)
             {
