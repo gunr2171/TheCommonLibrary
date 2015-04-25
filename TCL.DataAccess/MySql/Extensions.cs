2777a7a16135de1fd6700d12574272ad8c26d98c
gunr2171 <kevin@kevin-hunter.net>
2015-04-25 10:50:03 -0400

    moved the sql extensions to be in the DataAccess project this way they don't need to be in Extensions and that project doesn't need those nuget packages.

---
+++ TCL.DataAccess/MySql/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCL.DataAccess.MySql
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Adds a parameter to the parameter collection with it's accompanying value.
+        /// If the value passed in is null it will be converted to DBNull.Value.
+        /// </summary>
+        /// <param name="collection">The collection to add the parameter to.</param>
+        /// <param name="parameterName">The name of the parameter. Like "@ParamName".</param>
+        /// <param name="value">The value of the parameter.</param>
+        public static void AddParam(this MySqlParameterCollection collection, string parameterName, object value)
+        {
+            if (value == null)
+            {
+                collection.AddWithValue(parameterName, DBNull.Value);
+            }
+            else
+            {
+                collection.AddWithValue(parameterName, value);
+            }
+        }
+    }
+}
---
+++ TCL.DataAccess/Postgres/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCL.DataAccess.Postgres
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Adds a parameter to the parameter collection with it's accompanying value.
+        /// If the value passed in is null it will be converted to DBNull.Value.
+        /// </summary>
+        /// <param name="collection">The collection to add the parameter to.</param>
+        /// <param name="parameterName">The name of the parameter. Like ":ParamName". REMEMBER, the prefix for Postgres parameters are different than others!</param>
+        /// <param name="value">The value of the parameter.</param>
+        public static void AddParam(this NpgsqlParameterCollection collection, string parameterName, object value)
+        {
+            if (value == null)
+            {
+                collection.AddWithValue(parameterName, DBNull.Value);
+            }
+            else
+            {
+                collection.AddWithValue(parameterName, value);
+            }
+        }
+    }
+}
